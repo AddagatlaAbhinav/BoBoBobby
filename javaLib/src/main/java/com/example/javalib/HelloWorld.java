2f8a2015a271e52592a3ce00b016a308362c2a41
aravi <21BQ1A0501@gmail.com>
2023-01-28 11:27:53 +0530

    BoBoBobby App

---
+++ javaLib/src/main/java/com/example/javalib/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.javalib;
+
+import java.util.Locale;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        String s="hello \fjack";
+        System.out.println(s);
+    }
+}
